52563c431d04ca608013281d92f1f41074b44e86
goldeneggg <jpshadowapps@gmail.com>
2015-01-09 14:33:57 +0900

    remove invalid package

--- ubuntu14/php5-fpm.sh
+++ ubuntu14/php5-fpm.sh
@@ -16,7 +16,7 @@ bash ${MYDIR}/init_ja.sh
 ${PRVENV_CMD_PKG_INS} php5-fpm
 
 # install additional packages
-${PRVENV_CMD_PKG_INS} php5-cli php5-curl php5-gd php5-memcache php5-memcached php5-mysql php5-xml php5-xmlrpc
+${PRVENV_CMD_PKG_INS} php5-cli php5-curl php5-gd php5-memcache php5-memcached php5-mysql php5-xmlrpc
 
 # modify php.ini
 PHP_INI_FILE=/etc/php5/fpm/php.ini
