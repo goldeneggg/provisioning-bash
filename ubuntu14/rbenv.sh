5f7677abe0996cf32e34b7a2cf175ca9419396d2
goldeneggg <jpshadowapps@gmail.com>
2015-01-09 15:25:45 +0900

    add rbenv.sh

---
+++ ubuntu14/rbenv.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#>>>>>>>>>> prepare
+MYNAME=`basename $0`
+MYDIR=$(cd $(dirname $0) && pwd)
+MYUSER=$(whoami)
+
+# load environments
+source ${MYDIR}/envs
+#<<<<<<<<<<
+
+
+# args
+## 1 = envfile path(default: "~/.bashrc", if only non-privilledged user)
+ENV_RC=${PRVENV_DEFAULT_BASHRC}
+if [ ${MYUSER} != "root" ]
+then
+  echo "Executed by non-root user = ${MYUSER}"
+  ENV_RC=$HOME/.bashrc
+  if [ $# -eq 1 ]
+  then
+    ENV_RC=${1}
+    echo "ARGS(1) = envfile path = ${ENV_RC}"
+  fi
+fi
+
+# clone
+RBENV_ROOT=$HOME/.rbenv
+
+git clone https://github.com/sstephenson/rbenv.git ${RBENV_ROOT}
+
+# plugins
+cd ${RBENV_ROOT}
+mkdir plugins
+cd plugins
+git clone https://github.com/sstephenson/ruby-build.git
+
+# set environments
+echo "export PATH=${RBENV_ROOT}/bin"':$PATH' >> ${ENV_RC}
+#[[ -s "${RBENV_ROOT}/bin/rbenv" ]] && eval "$(rbenv init -)"
+echo 'eval "$(rbenv init -)"' >> ${ENV_RC}
+source ${ENV_RC}
+
+# confirm rbenv version
+rbenv --version
