aeb1f934209a18d607d9400ce4ccc0fd86519939
goldeneggg <jpshadowapps@gmail.com>
2015-03-23 21:13:41 +0900

    fix my script

--- ubuntu14/goldeneggg.sh
+++ ubuntu14/goldeneggg.sh
@@ -10,8 +10,21 @@ source ${MYDIR}/envs
 #<<<<<<<<<<
 
 
+# args
+## 1 = github user
+## 2 = github mail
+GITHUB_USER=""
+GITHUB_MAIL=""
+if [ $# -eq 2 ]
+then
+  GITHUB_USER=$1
+  echo "ARGS(1) = github user = ${GITHUB_USER}"
+  GITHUB_MAIL=$2
+  echo "ARGS(2) = github mail = ${GITHUB_MAIL}"
+fi
+
 # setup dotfiles
 cd ~
 git clone https://github.com/goldeneggg/dotfiles.git
 cd dotfiles
-bash setup.sh -L
+bash setup.sh -L --github-user ${GITHUB_USER} --github-mail ${GITHUB_MAIL}
