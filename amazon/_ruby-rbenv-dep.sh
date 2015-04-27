cb286cbf6c625dc57da4ba1a801e938ed5b595ae
goldeneggg <jpshadowapps@gmail.com>
2015-04-28 00:18:45 +0900

    add ruby for amazon

---
+++ amazon/_ruby-rbenv-dep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# install dependencies
+${PRVENV_CMD_PKG_INS} libffi-devel zlib-devel openssl-devel
---
+++ amazon/_ruby-rbenv-rails4-dep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+: "----- install package dependencies for rails4"
+${PRVENV_CMD_PKG_INS} sqlite sqlite-devel
