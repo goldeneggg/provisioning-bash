0b0d3bc98362905b26dfeb94164fd6642acf84f9
goldeneggg <jpshadowapps@gmail.com>
2015-05-17 04:52:25 +0900

    add rust.sh

---
+++ ubuntu14/rust.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#>>>>>>>>>> prepare
+source prepare.sh
+#<<<<<<<<<<
+
+
+set -e
+
+[ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}"; exit 1; }
+
+: "----- download erlang"
+declare -r MAJOR_VER=${1:-"1.0"}
+declare -r MINOR_VER=${2:-"0"}
+
+declare -r VER=${MAJOR_VER}.${MINOR_VER}
+declare -r TAR=rust-${VER}-x86_64-unknown-linux-gnu.tar.gz
+
+pushd ${PRVENV_INSTALL_WORK_DIR}
+[ -f ${TAR} ] && rm -f ${TAR}
+
+# https://static.rust-lang.org/dist/rust-1.0.0-x86_64-unknown-linux-gnu.tar.gz
+${PRVENV_WGETCMD} https://static.rust-lang.org/dist/${TAR}
+
+declare -r SRCDIR=${TAR//.tar.gz/}
+[ -d ${SRCDIR} ] && rm -fr ${SRCDIR}
+tar zxf ${TAR}
+
+: "----- install"
+pushd ${SRCDIR}
+
+sh install.sh
