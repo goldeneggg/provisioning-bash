9799740f755a5cb4ffcdfd83af1e04b40b7cdde8
goldeneggg <jpshadowapps@gmail.com>
2015-08-12 03:13:13 +0900

    add daemontools for ubuntu14

--- ubuntu14/cui.sh
+++ ubuntu14/cui.sh
@@ -8,4 +8,4 @@ source prepare.sh
 [ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}"; exit 1; }
 
 : "----- install packages for cui environment"
-${PRVENV_CMD_PKG_INS} ncurses-term screen tmux git subversion zsh vim ctags lv
+${PRVENV_CMD_PKG_INS} ncurses-term screen tmux git subversion zsh vim ctags lv daemontools
