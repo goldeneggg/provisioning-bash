2bf482ca2542ae6a385b112a7c0c1d1c2888a0ac
goldeneggg <jpshadowapps@gmail.com>
2016-10-13 15:11:35 +0900

    add td-agent.sh

---
+++ ubuntu16/td-agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#>>>>>>>>>> prepare
+source prepare.sh
+#<<<<<<<<<<
+
+
+[ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}" >&2; exit 1; }
+
+curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-xenial-td-agent2.sh | sh
+
+/etc/init.d/td-agent start
