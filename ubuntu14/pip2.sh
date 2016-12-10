8ec8a511ecddb1cd2eef614420f26b688bfb33d9
goldeneggg <jpshadowapps@gmail.com>
2016-12-10 21:25:54 +0900

    fix wget cmd

--- ubuntu14/pip2.sh
+++ ubuntu14/pip2.sh
@@ -7,7 +7,7 @@ source prepare.sh
 
 : "----- install pip"
 # (required: install python2)
-wget https://bootstrap.pypa.io/get-pip.py
+${PRVENV_WGETCMD} https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
 
 : "----- setup pip PATH"
