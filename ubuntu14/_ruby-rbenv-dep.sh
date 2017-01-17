0a566900831a851590c541a8d358cf48daf5e501
goldeneggg <jpshadowapps@gmail.com>
2017-01-17 14:47:39 +0900

    fix _ruby-rbenv-dev

--- ubuntu14/_ruby-rbenv-dep.sh
+++ ubuntu14/_ruby-rbenv-dep.sh
@@ -3,4 +3,4 @@
 
 : "----- install package dependencies for ruby"
 # FIXME mysql version
-${PRVENV_CMD_PKG_INS} libffi-dev zlib1g-dev libssl-dev libreadline-dev mysql-client-5.6 libmysqld-dev
+${PRVENV_CMD_PKG_INS} libffi-dev zlib1g-dev libssl-dev libreadline-dev mysql-client libmysqld-dev
