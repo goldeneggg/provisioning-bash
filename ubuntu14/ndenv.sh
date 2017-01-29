1f8222ae7bdf17be38af72716de0724073f93306
goldeneggg <jpshadowapps@gmail.com>
2017-01-29 16:33:44 +0900

    add ndenv and some fixes

---
+++ ubuntu14/ndenv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#>>>>>>>>>> prepare
+source prepare.sh
+#<<<<<<<<<<
+
+
+set -e
+
+declare -r MAJOR_VER=${1:-"6.9"}
+declare -r MINOR_VER=${2:-"2"}
+declare -r VER=${MAJOR_VER}.${MINOR_VER}
+
+: "----- install ndenv"
+declare -r NDENV_DIR=${HOME}/.ndenv
+[ -d ${NDENV_DIR} ] && rm -fr ${NDENV_DIR}
+git clone https://github.com/riywo/ndenv ~/.ndenv
+
+declare -r NDENV_BIN=${NDENV_DIR}/bin
+echo "export PATH=${NDENV_BIN}:${PATH}" >> ${ENV_RC}
+echo 'eval "$(ndenv init -)"' >> ${ENV_RC}
+set +u; source ${ENV_RC}; set -u
+
+: "----- install nodebuild"
+declare -r NDENV_PLUGIN_DIR=${NDENV_DIR}/plugins
+[ -d ${NDENV_PLUGIN_DIR} ] || mkdir ${NDENV_PLUGIN_DIR}
+git clone https://github.com/riywo/node-build.git ${NDENV_PLUGIN_DIR}/node-build
+
+: "----- install node.js using ndenv"
+${NDENV_BIN}/ndenv install v${VER}
+${NDENV_BIN}/ndenv rehash
+${NDENV_BIN}/ndenv global v${VER}
--- ubuntu14/nodebrew-src.sh
+++ ubuntu14/nodebrew-src.sh
@@ -7,6 +7,10 @@ source prepare.sh
 
 set -e
 
+declare -r MAJOR_VER=${1:-"6.9"}
+declare -r MINOR_VER=${2:-"2"}
+declare -r VER=${MAJOR_VER}.${MINOR_VER}
+
 : "----- install nodebrew"
 declare -r NODEBREW_DIR=${HOME}/.nodebrew
 [ -d ${NODEBREW_DIR} ] && rm -fr ${NODEBREW_DIR}
@@ -17,9 +21,5 @@ echo "export PATH=${NODEBREW_BIN}:${PATH}" >> ${ENV_RC}
 set +u; source ${ENV_RC}; set -u
 
 : "----- install node.js using nodebrew"
-declare -r MAJOR_VER=${1:-"6.9"}
-declare -r MINOR_VER=${2:-"2"}
-declare -r VER=${MAJOR_VER}.${MINOR_VER}
-
 ${NODEBREW_BIN}/nodebrew install-binary v${VER}
 ${NODEBREW_BIN}/nodebrew use v${VER}
