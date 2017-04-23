03adc81112d8f1831d45798e61b18e390d5461ca
goldeneggg <jpshadowapps@gmail.com>
2017-04-23 11:07:22 +0900

    add unzip

--- ubuntu14/cui.sh
+++ ubuntu14/cui.sh
@@ -8,4 +8,4 @@ source prepare.sh
 [ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}" >&2; exit 1; }
 
 : "----- install packages for cui environment"
-${PRVENV_CMD_PKG_INS} ncurses-term screen tmux git subversion zsh vim ctags lv daemontools gdb
+${PRVENV_CMD_PKG_INS} ncurses-term screen tmux git subversion zsh vim ctags lv daemontools gdb unzip
