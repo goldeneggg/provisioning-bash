a321a4d3328b7673ec38fb57a639e3e53f1121ed
goldeneggg <jpshadowapps@gmail.com>
2017-09-17 11:39:15 +0900

    fix mysql

--- ubuntu14/_mysql56-src-dep.sh
+++ ubuntu14/_mysql56-src-dep.sh
@@ -3,3 +3,6 @@
 
 : "----- install package dependencies for mysql"
 ${PRVENV_CMD_PKG_INS} build-essential cmake bison libncurses5-dev
+
+: "----- purge default mysql packages"
+${PRVENV_CMD_PKG_RMV_PRGE} "mysql-*"
