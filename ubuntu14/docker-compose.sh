18893cf94e0c9a0f9dc114ca2626eb6e0ccd379e
goldeneggg <jpshadowapps@gmail.com>
2017-10-28 17:23:00 +0900

    add docker-compose

---
+++ ubuntu14/docker-compose.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#>>>>>>>>>> prepare
+source prepare.sh
+#<<<<<<<<<<
+
+
+set -e
+
+[ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}" >&2; exit 1; }
+
+# args
+declare -r DOCKER_COMPOSE_VER=${1:-"1.16.1"}
+declare -r DOCKER_COMPOSE_PREFIX=/usr/local
+echo "docker-compose ver = ${DOCKER_COMPOSE_VER}"
+
+: "----- install docker-compose"
+curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VER}/docker-compose-$(uname -s)-$(uname -m)" -o ${DOCKER_COMPOSE_PREFIX}/bin/docker-compose
+chmod +x ${DOCKER_COMPOSE_PREFIX}/bin/docker-compose
