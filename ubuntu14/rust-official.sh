fd856be75edfc209391d6fc63bddf370ce77ccec
goldeneggg <jpshadowapps@gmail.com>
2017-11-07 00:50:33 +0900

    modified install URL

--- ubuntu14/rust-official.sh
+++ ubuntu14/rust-official.sh
@@ -10,4 +10,4 @@ set -e
 [ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}" >&2; exit 1; }
 
 : "----- install rust"
-curl -sSf https://static.rust-lang.org/rustup.sh | sh -s -- -y
+curl https://sh.rustup.rs -sSf | sh
