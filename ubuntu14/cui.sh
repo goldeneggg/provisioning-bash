552cd6471461cdaae7bc3e4e407e5f1d2ec3fc68
goldeneggg <jpshadowapps@gmail.com>
2018-02-12 15:39:57 +0900

    add neovim

--- ubuntu14/cui.sh
+++ ubuntu14/cui.sh
@@ -11,6 +11,12 @@ source prepare.sh
 ${PRVENV_CMD_PKG_INS} ncurses-term screen tmux git subversion zsh ctags lv daemontools gdb unzip
 
 : "----- install latest vim"
-add-apt-repository ppa:jonathonf/vim
+add-apt-repository -y ppa:jonathonf/vim
 ${PRVENV_CMD_PKG_UPD}
 ${PRVENV_CMD_PKG_INS} vim
+
+: "----- install latest neovim by appimage"
+declare -r NVIM_PREFIX=/usr/local
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod +x nvim.appimage
+mv nvim.appimage ${NVIM_PREFIX}/bin/nvim
