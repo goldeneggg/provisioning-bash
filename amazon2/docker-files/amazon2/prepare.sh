1c22c4f253a51dac1ac06cb86e93d636d179fd84
goldeneggg <jpshadowapps@gmail.com>
2019-03-05 15:28:16 +0900

    add amazon2

---
+++ amazon2/docker-files/amazon2/prepare.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### common prepare script. expect to be called `source prepare.sh`
+
+#set -eux
+set -ux
+
+export MYNAME=`basename $0`
+export MYDIR=$(cd $(dirname $0) && pwd)
+export MYUSER=$(whoami)
+export MYLOGDIR=${MYDIR}/logs
+export MYLOG=${MYLOGDIR}/${MYNAME}.log
+
+source ${MYDIR}/envs
+
+function isroot {
+  [ ${MYUSER} = "root" ] && echo "yes"
+}
+
+[ $(isroot) ] \
+  && { export ENV_RC=${PRVENV_DEFAULT_BASHRC}; } \
+  || { export ENV_RC=${PRVENV_USER_BASHRC}; }
