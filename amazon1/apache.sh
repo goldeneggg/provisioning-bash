652b6690c3f4594d3a4e95c328c85f4257ed7e5d
goldeneggg <jpshadowapps@gmail.com>
2019-03-17 15:31:10 +0900

    add httpd for amazon

---
+++ amazon1/apache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#>>>>>>>>>> prepare
+source prepare.sh
+#<<<<<<<<<<
+
+
+[ $(isroot) ] || { echo "${MYUSER} can not run ${MYNAME}"; exit 1; }
+
+${PRVENV_CMD_PKG_INS} httpd
+
+service httpd start
---
+++ amazon2/nginx.sh
@@ -0,0 +1,1 @@
+../amazon1/nginx.sh
